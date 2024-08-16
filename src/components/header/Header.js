0c66e03946859a4351bf00a5884181d6c85a587c
Sang phuoc <sangphuoclam@gmail.com>
2024-08-16 22:36:32 +0700

    headerv1

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,8 +1,19 @@
+import React from 'react';
+import { Container, Box } from '@mui/material';
 
-import React from "react";
-
-export function Header(){
-    return (
-        <h1>hello</h1>
-    );
+export default function Header() {
+  return (
+    <Container 
+      maxWidth="xxl" // You can change "lg" to any breakpoint ("xs", "sm", "md", "lg", "xl")
+      disableGutters sx={{ backgroundColor: "black" }}
+    >
+      <Box 
+        sx={{ 
+          bgcolor: "rgb(18, 18, 18)", 
+          height: "10vh", 
+          width: "100%" 
+        }} 
+      />
+    </Container>
+  );
 }
