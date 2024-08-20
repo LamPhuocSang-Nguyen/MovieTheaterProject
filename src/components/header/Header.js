7c10ff045e7aa65c4b1e8a03dd9aa7ce34282e6c
Sang phuoc <sangphuoclam@gmail.com>
2024-08-20 07:05:27 +0700

    header

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,19 +1,54 @@
-import React from 'react';
-import { Container, Box } from '@mui/material';
+import React from "react";
+import { Container, Box } from "@mui/material";
+import AppBar from "@mui/material/AppBar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import MenuIcon from "@mui/icons-material/Menu";
+import SearchIcon from "@mui/icons-material/Search";
+import MoreIcon from "@mui/icons-material/MoreVert";
+import { styled } from "@mui/material/styles";
+import Toolbar from "@mui/material/Toolbar";
 
 export default function Header() {
+  const StyledToolbar = styled(Toolbar)(({ theme }) => ({
+    alignItems: "flex-start",
+    paddingTop: theme.spacing(1),
+    paddingBottom: theme.spacing(2),
+    backgroundColor: "rgb(18, 18, 18)",
+    // Override media queries injected by theme.mixins.toolbar
+    "@media all": {
+      minHeight: 128,
+    },
+  }));
+
   return (
-    <Container 
+    <Container
       maxWidth="xxl" // You can change "lg" to any breakpoint ("xs", "sm", "md", "lg", "xl")
-      disableGutters sx={{ backgroundColor: "black" }}
+      disableGutters
     >
-      <Box 
-        sx={{ 
-          bgcolor: "rgb(18, 18, 18)", 
-          height: "10vh", 
-          width: "100%" 
-        }} 
-      />
+      <AppBar position="static">
+        <StyledToolbar>
+          <Typography
+            variant="h5"
+            noWrap
+            component="div"
+            sx={{ flexGrow: 1, alignSelf: "flex-end" }}
+          >
+            MUI
+          </Typography>
+          <IconButton size="large" aria-label="search" color="inherit">
+            <SearchIcon />
+          </IconButton>
+          <IconButton
+            size="large"
+            aria-label="display more actions"
+            edge="end"
+            color="inherit"
+          >
+            <MoreIcon />
+          </IconButton>
+        </StyledToolbar>
+      </AppBar>
     </Container>
   );
 }
