84590908c6362a6af4779fd71711a453512f0ea6
Sang phuoc <sangphuoclam@gmail.com>
2024-09-23 23:42:07 +0700

    updatemaxWidth

--- src/components/media/Media.js
+++ src/components/media/Media.js
@@ -1,48 +1,62 @@
-import { Container, Typography } from '@mui/material';
-import Grid from '@mui/material/Grid';
+import { Container, Typography } from "@mui/material";
+import Grid from "@mui/material/Grid";
 import React from "react";
-import { styled } from '@mui/material/styles';
-import Paper from '@mui/material/Paper';
-import Box from '@mui/material/Box';
+import { styled } from "@mui/material/styles";
+import Paper from "@mui/material/Paper";
+import Box from "@mui/material/Box";
 import AOS from "aos";
 import "aos/dist/aos.css";
-import { useEffect } from 'react';
+import { useEffect } from "react";
 
 const Item = styled(Paper)(({ theme }) => ({
-    backgroundColor: '#fff',
-    ...theme.typography.body2,
-    padding: theme.spacing(1),
-    textAlign: 'center',
-    color: theme.palette.text.secondary,
-    ...theme.applyStyles('dark', {
-      backgroundColor: '#1A2027',
-    }),
-  }));
+  backgroundColor: "#fff",
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: "center",
+  color: theme.palette.text.secondary,
+  ...theme.applyStyles("dark", {
+    backgroundColor: "#1A2027",
+  }),
+}));
 
-export default function Media(props){
-    useEffect(() => {
-        AOS.init();
-        AOS.refresh();
-      }, []);
-    const heading = props.heading;
-    const content = props.content;
-    const right = props.right;
-    const image = props.image;
-    const youtobeId = props.youtobeId;
+export default function Media(props) {
+  useEffect(() => {
+    AOS.init({duration:1000});
+    AOS.refresh();
+  }, []);
 
-    return(
-        <Container maxWidth="xl">
-            <Grid container>
-                <Grid Item data-aos={right ? "fade-right": "fade-left"}>
-                    <Typography sx={{color:"white"}}>{heading}</Typography>
-                    <Typography sx={{color:"white"}}>{content}</Typography>
-                </Grid>
-                <Grid Item data-aos="fade-right">
-                    <Box component="img"
-                    src={image}>
-                    </Box>
-                </Grid>
-            </Grid>
-        </Container>
-    )
+  const heading = props.heading;
+  const content = props.content;
+  const right = props.right;
+  const image = props.image;
+  const youtobeId = props.youtobeId;
+
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <Grid container>
+        <Grid
+          Item
+          data-aos="fade-right"
+          xs={12}
+          sm={6}
+          md={6}
+          lg={6}
+          xl={6}
+        >
+          <Typography sx={{ color: "white" }}>{heading}</Typography>
+          <Typography sx={{ color: "white" }}>{content}</Typography>
+        </Grid>
+        <Grid Item data-aos="fade-left" xs={12} sm={6} md={6} lg={6} xl={6}>
+          <Box
+            component="img"
+            src={image}
+            style={{
+              width: "100%",
+              height: "100%",
+            }}
+          ></Box>
+        </Grid>
+      </Grid>
+    </Box>
+  );
 }
--- src/pages/LandingPage/LandingPage.js
+++ src/pages/LandingPage/LandingPage.js
@@ -6,7 +6,7 @@ import Media from "../../components/media/Media";
 
 export default function LandingPage(){
     return(
-        <Container maxWidth="xl" disableGutters sx={{backgroundColor:"rgb(18, 18, 18)"}}>
+        <Container maxWidth="xxl" disableGutters sx={{backgroundColor:"rgb(18, 18, 18)"}}>
             <Box sx={{padding:{xs:4, sm:8, md:8, lg:10}, color:"white", display:"flex", alignItems:"center", flexDirection:"column"}}>
                 <Typography textAlign="center" gutterBottom mb={6} sx={{ fontWeight: 900, fontSize : {xs: "30px", sm: "45px", md:"53px", lg: "60px"}}} variant='h2'>Unlimited anime movies, shows and more</Typography>
                 <Typography textAlign="center" gutterBottom mb={6} sx={{ fontWeight: 400, fontSize : {xs: "25px", sm: "30px", md:"43px", lg: "50px"} }} variant='h3'>Watch anywhere. Cancel anytime</Typography>
