cf32e8966aa7dbc604548e26b773f8202d890a05
LamPhuocSang-Nguyen <75714558+LamPhuocSang-Nguyen@users.noreply.github.com>
2024-11-18 23:39:40 +0700

    update Landingpage

--- src/components/media/Media.js
+++ src/components/media/Media.js
@@ -32,37 +32,97 @@ export default function Media(props) {
   const youtobeId = props.youtobeId;
 
   return (
-    <Container maxWidth="100%">
-      <Grid container>
-        <Grid
-          Item
-          data-aos={right ? "fade-right" : "fade-left"}
-          xs={12}
-          sm={6}
-          md={6}
-          lg={6}
-          xl={6}
-          sx={{
-            maxWidth: "100%",
-            maxHeight: "100%",
-            display: {xs: "block", md : right ? "block" : "none"}
-          }}
-        >
-          <Typography variant='h3' textAlign="center" mb={6} sx={{ color: "white" }}>{heading}</Typography>
-          <Typography variant='h5' textAlign="center" sx={{ color: "white" }}>{content}</Typography>
+      <Container maxWidth="lg" sx={{ overflow: 'hidden' }}>
+        <Grid container columnSpacing={2} rowSpacing={4}>
+          <Grid item xs={12} md={7}
+                data-aos="fade-right"
+                sx={{
+                  display: { xs: "none", md: right ? "none" : "block" },
+                  position: 'relative',
+                  overflow: 'hidden',
+                }}>
+            <Box
+                sx={{
+                  position: 'relative',
+                  width: '100%',
+                  height: '100%',
+                  display: 'flex',
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                }}
+            >
+              <img
+                  src={image}
+                  alt={heading}
+                  style={{
+                    width: '100%',
+                    height: '100%',
+                    objectFit: 'contain',
+                    position: 'relative',
+                    zIndex: 0
+                  }}
+              />
+            </Box>
+          </Grid>
+          <Grid item xs={12} md={5}
+                data-aos={right ? "fade-right" : "fade-left"}
+                sx={{
+                  display: "flex",
+                  justifyContent: "center",
+                  alignItems: "center",
+                  flexDirection: "column",
+                    color: "white"
+                }}>
+            <Typography variant='h3' textAlign="center" mb={6}>{heading}</Typography>
+            <Typography variant='h5' textAlign="center">{content}</Typography>
+          </Grid>
+          <Grid item xs={12} md={7}
+                data-aos="fade-left"
+                sx={{
+                  display: { xs: "block", md: right ? "block" : "none" },
+                  position: 'relative',
+                  overflow: 'hidden',
+                }}>
+            <Box
+                sx={{
+                  position: 'relative',
+                  width: '100%',
+                  height: '100%',
+                  display: 'flex',
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                }}
+            >
+              <img
+                  src={image}
+                  alt={heading}
+                  style={{
+                    width: '100%',
+                    height: '100%',
+                    objectFit: 'contain',
+                    position: 'relative',
+                    zIndex: 0
+                  }}
+              />
+              {youtobeId && <iframe
+                  src={`https://www.youtube.com/embed/${youtobeId}?autoplay=1&loop=1&playlist=${youtobeId}&mute=1&controls=0&showinfo=0&rel=0`}
+                  frameBorder="0"
+                  allow="autoplay; encrypted-media"
+                  allowFullScreen
+                  style={{
+                    position: 'absolute',
+                    top: '30%',
+                    left: '50%',
+                    transform: 'translate(-50%, -50%)',
+                    width: '60%',
+                    height: '40%',
+                    objectFit: 'cover',
+                    zIndex: -1
+                  }}
+              />}
+            </Box>
+          </Grid>
         </Grid>
-        <Grid Item data-aos={right ? "fade-right" : "fade-left"}
-              xs={12} sm={6} md={6} lg={6} xl={6}>
-          <Box
-            component="img"
-            src={image}
-            sx={{
-              maxWidth: "100%",
-              maxHeight: "100%",
-            }}
-          ></Box>
-        </Grid>
-      </Grid>
-    </Container>
+      </Container>
   );
 }
